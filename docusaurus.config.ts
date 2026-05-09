e3bf4dddeecb62cd1543222cf4f98350f159bdc0
Edgar Lakshin <edgarlakshin@Mac.lan>
2026-05-09 12:36:51 +0200

    changed baseurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
   // },
 
   // Set the production url of your site here
-  url: 'https://datacloudhero.com',
+  url: 'https://blog.datacloudhero.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
