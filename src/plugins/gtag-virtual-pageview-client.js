a3ac06a725bd01d0a5020b2afccb8e668803e90d
Edgar Lakshin <edgarlakshin@Mac.lan>
2026-05-10 19:30:50 +0200

    clientModule

--- src/plugins/gtag-virtual-pageview-client.js
+++ src/plugins/gtag-virtual-pageview-client.js
@@ -3,18 +3,34 @@ import { useLocation } from '@docusaurus/router';
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
 if (ExecutionEnvironment.canUseDOM) {
-  // Функция для отправки виртуального просмотра
+  console.log('[GTM] Client module loaded');
+  
   window.trackVirtualPageView = function trackVirtualPageView() {
+    console.log('[GTM] trackVirtualPageView called');
+    console.log('[GTM] Has consent:', window.CookieConsent?.acceptedCategory('analytics'));
+    console.log('[GTM] Has gtag:', !!window.gtag);
+    console.log('[GTM] GTM loaded:', window.__gtmLoaded);
+
     const hasConsent = 
       window.CookieConsent?.acceptedCategory('analytics') || 
       window.__gtmLoaded;
 
-    if (!hasConsent || !window.gtag) {
+    if (!hasConsent) {
+      console.log('[GTM] No consent or GTM not loaded, skipping');
       return;
     }
 
-    // Небольшая задержка для корректного обновления DOM и title
+    if (!window.gtag) {
+      console.log('[GTM] window.gtag not available');
+      return;
+    }
+
     setTimeout(() => {
+      console.log('[GTM] Sending page_view event', {
+        page_path: window.location.pathname,
+        page_title: document.title,
+      });
+
       window.gtag('event', 'page_view', {
         page_path: window.location.pathname,
         page_location: window.location.href,
@@ -23,7 +39,6 @@ if (ExecutionEnvironment.canUseDOM) {
     }, 0);
   };
 
-  // Слушайте изменение маршрута
   window.addEventListener('popstate', window.trackVirtualPageView);
 }
 
@@ -31,6 +46,8 @@ export default function gtmVirtualPageView(props) {
   const location = useLocation();
 
   useEffect(() => {
+    console.log('[GTM] Route changed to:', location.pathname);
+    
     if (ExecutionEnvironment.canUseDOM && window.trackVirtualPageView) {
       window.trackVirtualPageView();
     }
