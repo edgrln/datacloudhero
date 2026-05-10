664984e9f4ccba237e6e0759dc0ae40cc9612bf0
Edgar Lakshin <edgarlakshin@Mac.lan>
2026-05-10 15:15:42 +0200

    cookies file

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -155,7 +155,7 @@ const config: Config = {
               to: '/cookies',
             },
             {
-              html: '<button type="button" data-cc="show-preferencesModal" class="footer__link clean-btn">Cookie settings</button>',
+              html: '<button type="button" data-cc="show-preferencesModal" class="footer__link cookie-settings-btn">Cookie settings</button>',
             },
           ],
         },
