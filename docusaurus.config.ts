48b60ec8279d72613514d437629c157fc47fc0fc
Edgar Lakshin <edgarlakshin@Edgars-MacBook-Pro-2.local>
2026-05-26 19:12:37 +0200

    Cookie Pilicy Page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -182,7 +182,7 @@ const config: Config = {
               to: '/cookies',
             },
             {
-              html: '<button type="button" class="footer__link cookie-settings-btn">Cookie settings</button>',
+            html: '<button type="button" class="footer__link-item cookie-settings-btn">Cookie settings</button>',
             },
           ],
         },
